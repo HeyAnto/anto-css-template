d41b659deca4de5700970ca5e2e10dc486fa51bb
HeyAnto <antonin63170@gmail.com>
2025-03-25 09:50:12 +0100

    Add Header

--- components/header.php
+++ components/header.php
@@ -1,3 +1,11 @@
-<header>
+<header class="flex justify-end p-10 gap-10">
+    <a class="btn btn-download flex gap-3" href="">
+        <img src="/assets/icons/icon-download.svg" alt="Télécharger">
+        Télécharger
+    </a>
 
+    <a class="btn btn-primary flex gap-3" href="">
+        <img src="/assets/icons/icon-github.svg" alt="GitHub">
+        GitHub
+    </a>
 </header>
