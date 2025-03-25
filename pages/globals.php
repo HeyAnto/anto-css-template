424e1edd161e5fd35ec27c920106eb6f3a816142
HeyAnto <antonin63170@gmail.com>
2025-03-25 21:44:21 +0100

    Add Globals Page

---
+++ pages/globals.php
@@ -0,0 +1,20 @@
+<?php include_once "../components/head.php" ?>
+
+<section class="flex flex-column gap-5">
+    <h1>
+        <span>
+            <img src="/assets/images/emoji/emoji-paint.webp" alt="Emoji Paint">
+        </span>
+        Globals
+    </h1>
+    <p>
+        Ce fichier définit les styles globaux du projet.
+        Il contient les imports des autres fichiers CSS,
+        la déclaration des polices de caractères,
+        ainsi que les variables CSS pour les couleurs,
+        les tailles de texte, les espacements et les styles de base pour les éléments HTML
+        (titres, paragraphes, liens, boutons...).
+    </p>
+</section>
+
+<?php include_once "../components/footer.php" ?>
